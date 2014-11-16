63f39a44f3adadc03df9486b3bd15c2dd98fcddd
Dave Ross <dave@davidmichaelross.com>
2014-11-15 19:21:33 -0500

    Tests for what happens when scrolling back to the top of the page

--- tests/spec/basictestsSpec.js
+++ tests/spec/basictestsSpec.js
@@ -23,3 +23,64 @@ describe('waypoint-once', function () {
 
 
 });
+
+describe('waypoint-visible', function () {
+
+	beforeEach(function (done) {
+
+		JSWaypoints.register_selector('#paragraph_20');
+		JSWaypoints.register_selector('#paragraph_40');
+		window.location.hash = 'paragraph_20';
+
+		setTimeout(function () {
+			done();
+		}, 1000);
+
+	});
+
+	it('added to element when it becomes visible', function () {
+		console.log(document.querySelector('#paragraph_20').className.split(/\s+/));
+		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-visible');
+	});
+
+	it('element below the viewport does not get waypoint-visible class', function () {
+		expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
+	});
+
+});
+
+describe('no longer visible', function () {
+
+	beforeEach(function (done) {
+
+		JSWaypoints.register_selector('#paragraph_20');
+		JSWaypoints.register_selector('#paragraph_40');
+		window.location.hash = 'paragraph_20';
+
+		// Give arrive a chance to run
+		setTimeout(function () {
+
+			window.location.hash = 'paragraph_1';
+
+			// Give arrive a chance to run again
+			setTimeout(function () {
+				done();
+			}, 1000);
+
+		}, 1000);
+
+	});
+
+	it('removed from element when no longer visible', function () {
+		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).not.toContain('waypoint-visible');
+	});
+
+	it('still has waypoint-once', function () {
+		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
+	});
+
+	it('element below the viewport never had waypoint-visible class', function () {
+		expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
+	});
+
+});
