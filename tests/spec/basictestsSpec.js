9e27ef540109eebc5abc28a896e6fd9cd4725f18
Dave Ross <dave@davidmichaelross.com>
2014-11-17 20:48:31 -0500

    Try wrapping the test code in a DOMContentLoaded handler to see if that's why it's failing tests on Travis-CI

--- tests/spec/basictestsSpec.js
+++ tests/spec/basictestsSpec.js
@@ -20,272 +20,276 @@ function setUpHTMLFixture() {
 
 }
 
-describe('waypoint-once', function () {
+window.addEventListener('DOMContentLoaded', function () {
 
-	beforeEach(function (done) {
+	describe('waypoint-once', function () {
 
-		window.location.hash = 'paragraph_1';
-		Arrive.reset();
-		setUpHTMLFixture();
-		Arrive.register_selector('#paragraph_20');
-		Arrive.register_selector('#paragraph_40');
-		window.location.hash = 'paragraph_19';
-
-		setTimeout(function () {
-			done();
-		}, 1000);
-
-	});
-
-	it('added to element when it becomes visible', function () {
-		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
-	});
-
-	it('element below the viewport does not get waypoint-once class', function () {
-		expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
-	});
-
-
-});
-
-describe('waypoint-visible', function () {
-
-	beforeEach(function (done) {
-
-		window.location.hash = 'paragraph_1';
-		Arrive.reset();
-		setUpHTMLFixture();
-		Arrive.register_selector('#paragraph_20');
-		Arrive.register_selector('#paragraph_40');
-		window.location.hash = 'paragraph_19';
-
-		setTimeout(function () {
-			done();
-		}, 1000);
-
-	});
-
-	it('added to element when it becomes visible', function () {
-		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-visible');
-	});
-
-	it('element below the viewport does not get waypoint-visible class', function () {
-		expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
-	});
-
-});
-
-describe('no longer visible', function () {
-
-	beforeEach(function (done) {
-
-		window.location.hash = 'paragraph_1';
-		Arrive.reset();
-		setUpHTMLFixture();
-		Arrive.register_selector('#paragraph_20');
-		Arrive.register_selector('#paragraph_40');
-		window.location.hash = 'paragraph_19';
-
-		// Give arrive a chance to run
-		setTimeout(function () {
+		beforeEach(function (done) {
 
 			window.location.hash = 'paragraph_1';
+			Arrive.reset();
+			setUpHTMLFixture();
+			Arrive.register_selector('#paragraph_20');
+			Arrive.register_selector('#paragraph_40');
+			window.location.hash = 'paragraph_19';
 
-			// Give arrive a chance to run again
 			setTimeout(function () {
 				done();
 			}, 1000);
 
-		}, 1000);
-
-	});
-
-	it('removed from element when no longer visible', function () {
-		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).not.toContain('waypoint-visible');
-	});
-
-	it('still has waypoint-once', function () {
-		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
-	});
-
-	it('element below the viewport never had waypoint-visible class', function () {
-		expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
-	});
-
-});
-
-describe('callbacks', function () {
-
-	var visible_callback_called,
-		no_longer_visible_callback_called;
-
-	beforeEach(function (done) {
-
-		visible_callback_called = {'20': false, '40': false};
-		no_longer_visible_callback_called = {'20': false, '40': false};
-
-		window.location.hash = 'paragraph_1';
-		Arrive.reset();
-		setUpHTMLFixture();
-		Arrive.register_selector('#paragraph_20', function () {
-			visible_callback_called['20'] = true;
-		}, function () {
-			no_longer_visible_callback_called['20'] = true;
-		});
-		Arrive.register_selector('#paragraph_40', function () {
-			visible_callback_called['40'] = true;
-		}, function () {
-			no_longer_visible_callback_called['40'] = true;
 		});
 
-		window.location.hash = 'paragraph_19';
+		it('added to element when it becomes visible', function () {
+			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
+		});
 
-		// Give arrive a chance to run
-		setTimeout(function () {
+		it('element below the viewport does not get waypoint-once class', function () {
+			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
+		});
+
+
+	});
+
+	describe('waypoint-visible', function () {
+
+		beforeEach(function (done) {
 
 			window.location.hash = 'paragraph_1';
+			Arrive.reset();
+			setUpHTMLFixture();
+			Arrive.register_selector('#paragraph_20');
+			Arrive.register_selector('#paragraph_40');
+			window.location.hash = 'paragraph_19';
 
-			// Give arrive a chance to run again
 			setTimeout(function () {
 				done();
 			}, 1000);
 
-		}, 1000);
+		});
+
+		it('added to element when it becomes visible', function () {
+			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-visible');
+		});
+
+		it('element below the viewport does not get waypoint-visible class', function () {
+			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
+		});
 
 	});
 
-	it('called the visible callback for paragraph 20', function () {
-		expect(visible_callback_called['20']).toBe(true);
-	});
+	describe('no longer visible', function () {
 
-	it('called the no-longer-visible callback for paragraph 20', function () {
-		expect(no_longer_visible_callback_called['20']).toBe(true);
-	});
-
-	it('never called the visible callback for paragraph 40', function () {
-		expect(visible_callback_called['40']).toBe(false);
-	});
-
-	it('never called the no-longer-visible callback for paragraph 40', function () {
-		expect(no_longer_visible_callback_called['40']).toBe(false);
-	})
-
-});
-
-describe('callback promises', function () {
-
-	var visible_callback_called,
-		no_longer_visible_callback_called;
-
-	beforeEach(function (done) {
-
-		visible_callback_called = {'20': false};
-		no_longer_visible_callback_called = {'20': false};
-
-		window.location.hash = 'paragraph_1';
-		Arrive.reset();
-		setUpHTMLFixture();
-		var promise = Arrive.register_selector('#paragraph_20');
-
-		promise.visible_callback = function () {
-			visible_callback_called['20'] = true;
-		};
-
-		promise.no_longer_visible_callback = function () {
-			no_longer_visible_callback_called['20'] = true;
-		};
-
-		window.location.hash = 'paragraph_19';
-
-		// Give arrive a chance to run
-		setTimeout(function () {
+		beforeEach(function (done) {
 
 			window.location.hash = 'paragraph_1';
+			Arrive.reset();
+			setUpHTMLFixture();
+			Arrive.register_selector('#paragraph_20');
+			Arrive.register_selector('#paragraph_40');
+			window.location.hash = 'paragraph_19';
 
-			// Give arrive a chance to run again
+			// Give arrive a chance to run
+			setTimeout(function () {
+
+				window.location.hash = 'paragraph_1';
+
+				// Give arrive a chance to run again
+				setTimeout(function () {
+					done();
+				}, 1000);
+
+			}, 1000);
+
+		});
+
+		it('removed from element when no longer visible', function () {
+			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).not.toContain('waypoint-visible');
+		});
+
+		it('still has waypoint-once', function () {
+			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
+		});
+
+		it('element below the viewport never had waypoint-visible class', function () {
+			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
+		});
+
+	});
+
+	describe('callbacks', function () {
+
+		var visible_callback_called,
+			no_longer_visible_callback_called;
+
+		beforeEach(function (done) {
+
+			visible_callback_called = {'20': false, '40': false};
+			no_longer_visible_callback_called = {'20': false, '40': false};
+
+			window.location.hash = 'paragraph_1';
+			Arrive.reset();
+			setUpHTMLFixture();
+			Arrive.register_selector('#paragraph_20', function () {
+				visible_callback_called['20'] = true;
+			}, function () {
+				no_longer_visible_callback_called['20'] = true;
+			});
+			Arrive.register_selector('#paragraph_40', function () {
+				visible_callback_called['40'] = true;
+			}, function () {
+				no_longer_visible_callback_called['40'] = true;
+			});
+
+			window.location.hash = 'paragraph_19';
+
+			// Give arrive a chance to run
+			setTimeout(function () {
+
+				window.location.hash = 'paragraph_1';
+
+				// Give arrive a chance to run again
+				setTimeout(function () {
+					done();
+				}, 1000);
+
+			}, 1000);
+
+		});
+
+		it('called the visible callback for paragraph 20', function () {
+			expect(visible_callback_called['20']).toBe(true);
+		});
+
+		it('called the no-longer-visible callback for paragraph 20', function () {
+			expect(no_longer_visible_callback_called['20']).toBe(true);
+		});
+
+		it('never called the visible callback for paragraph 40', function () {
+			expect(visible_callback_called['40']).toBe(false);
+		});
+
+		it('never called the no-longer-visible callback for paragraph 40', function () {
+			expect(no_longer_visible_callback_called['40']).toBe(false);
+		})
+
+	});
+
+	describe('callback promises', function () {
+
+		var visible_callback_called,
+			no_longer_visible_callback_called;
+
+		beforeEach(function (done) {
+
+			visible_callback_called = {'20': false};
+			no_longer_visible_callback_called = {'20': false};
+
+			window.location.hash = 'paragraph_1';
+			Arrive.reset();
+			setUpHTMLFixture();
+			var promise = Arrive.register_selector('#paragraph_20');
+
+			promise.visible_callback = function () {
+				visible_callback_called['20'] = true;
+			};
+
+			promise.no_longer_visible_callback = function () {
+				no_longer_visible_callback_called['20'] = true;
+			};
+
+			window.location.hash = 'paragraph_19';
+
+			// Give arrive a chance to run
+			setTimeout(function () {
+
+				window.location.hash = 'paragraph_1';
+
+				// Give arrive a chance to run again
+				setTimeout(function () {
+					done();
+				}, 1000);
+
+			}, 1000);
+
+		});
+
+		it('called the visible callback for paragraph 20', function () {
+			expect(visible_callback_called['20']).toBe(true);
+		});
+
+		it('called the no-longer-visible callback for paragraph 20', function () {
+			expect(no_longer_visible_callback_called['20']).toBe(true);
+		});
+
+	});
+
+	describe('register element', function () {
+
+		beforeEach(function (done) {
+
+			window.location.hash = 'paragraph_1';
+			Arrive.reset();
+			setUpHTMLFixture();
+			Arrive.register_element(document.querySelector('#paragraph_20'));
+			window.location.hash = 'paragraph_19';
+
 			setTimeout(function () {
 				done();
 			}, 1000);
 
-		}, 1000);
+		});
+
+		it('added to element when it becomes visible', function () {
+			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
+		});
+
+		it('element below the viewport does not get waypoint-once class', function () {
+			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
+		});
 
 	});
 
-	it('called the visible callback for paragraph 20', function () {
-		expect(visible_callback_called['20']).toBe(true);
-	});
+	describe('exceptions', function () {
 
-	it('called the no-longer-visible callback for paragraph 20', function () {
-		expect(no_longer_visible_callback_called['20']).toBe(true);
-	});
+		beforeEach(function (done) {
 
-});
-
-describe('register element', function () {
-
-	beforeEach(function (done) {
-
-		window.location.hash = 'paragraph_1';
-		Arrive.reset();
-		setUpHTMLFixture();
-		Arrive.register_element(document.querySelector('#paragraph_20'));
-		window.location.hash = 'paragraph_19';
-
-		setTimeout(function () {
+			window.location.hash = 'paragraph_1';
+			Arrive.reset();
+			setUpHTMLFixture();
 			done();
-		}, 1000);
 
-	});
+		});
 
-	it('added to element when it becomes visible', function () {
-		expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
-	});
+		it('throws an exception when not passed a string', function () {
 
-	it('element below the viewport does not get waypoint-once class', function () {
-		expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
+			var exception_received = false;
+
+			try {
+				Arrive.register_selector(document.getElementById('paragraph_30'));
+			}
+			catch (e) {
+				exception_received = true;
+			}
+
+			expect(exception_received).toBe(true);
+
+		});
+
+		it('throws an exception when not passed an element', function () {
+
+			var exception_received = false;
+
+			try {
+				Arrive.register_element('this is a string');
+			}
+			catch (e) {
+				exception_received = true;
+			}
+
+			expect(exception_received).toBe(true);
+
+		});
+
 	});
 
 });
-
-describe('exceptions', function () {
-
-	beforeEach(function (done) {
-
-		window.location.hash = 'paragraph_1';
-		Arrive.reset();
-		setUpHTMLFixture();
-		done();
-
-	});
-
-	it('throws an exception when not passed a string', function () {
-
-		var exception_received = false;
-
-		try {
-			Arrive.register_selector(document.getElementById('paragraph_30'));
-		}
-		catch (e) {
-			exception_received = true;
-		}
-
-		expect(exception_received).toBe(true);
-
-	});
-
-	it('throws an exception when not passed an element', function () {
-
-		var exception_received = false;
-
-		try {
-			Arrive.register_element('this is a string');
-		}
-		catch (e) {
-			exception_received = true;
-		}
-
-		expect(exception_received).toBe(true);
-
-	});
-
-})
