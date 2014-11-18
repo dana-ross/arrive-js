14867a88189acbb31044a8771449afde2c6a69b8
Dave Ross <dave@davidmichaelross.com>
2014-11-17 21:53:15 -0500

    Don't destroy the old fixture, create a new one

--- tests/spec/basictestsSpec.js
+++ tests/spec/basictestsSpec.js
@@ -1,21 +1,16 @@
-function setUpHTMLFixture() {
-
-	// Destroy the existing fixture if there is one
-	if (document.getElementById('fixture')) {
-		document.body.removeChild(document.getElementById('fixture'));
-	}
+function setUpHTMLFixture(prefix) {
 
 	// Create a new fixture div
 	var div = document.createElement('div');
-	div.id = 'fixture';
+	div.id = prefix + 'fixture';
 	document.body.appendChild(div);
 
 	// Populate the fixture div with a large number of lorem ipsum paragraphs
 	for (var paragraph_count = 1; paragraph_count <= 50; paragraph_count += 1) {
 		var paragraph = document.createElement('p');
-		paragraph.id = 'paragraph_' + paragraph_count;
+		paragraph.id = prefix + 'paragraph_' + paragraph_count;
 		paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porta mauris est, eget egestas elit luctus sed. Aliquam sagittis risus eget dui laoreet, id placerat augue tincidunt. Cras elementum purus a pellentesque molestie. Integer sagittis dictum feugiat. Aliquam a lacinia nibh. Cras eu nunc at lacus rutrum viverra sit amet vitae dolor. Vivamus a vehicula diam. Mauris nec enim purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin placerat, velit eget aliquet commodo, est orci dictum lectus, vitae mattis nisi nisi nec purus. Quisque blandit sem at ligula convallis, quis ornare nulla molestie. Donec molestie nulla in mattis interdum. Morbi in tempus ex. Donec pretium aliquet magna, sit amet malesuada turpis dictum sed.';
-		document.getElementById('fixture').appendChild(paragraph);
+		div.appendChild(paragraph);
 	}
 
 }
@@ -26,12 +21,11 @@ window.addEventListener('DOMContentLoaded', function () {
 
 		beforeEach(function (done) {
 
-			window.location.hash = 'paragraph_1';
 			Arrive.reset();
-			setUpHTMLFixture();
-			Arrive.register_selector('#paragraph_20');
-			Arrive.register_selector('#paragraph_40');
-			window.location.hash = 'paragraph_19';
+			setUpHTMLFixture('fixture1_');
+			Arrive.register_selector('#fixture1_paragraph_20');
+			Arrive.register_selector('#fixture1_paragraph_40');
+			window.location.hash = 'fixture1_paragraph_20';
 
 			setTimeout(function () {
 				done();
@@ -40,11 +34,11 @@ window.addEventListener('DOMContentLoaded', function () {
 		});
 
 		it('added to element when it becomes visible', function () {
-			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
+			expect(document.querySelector('#fixture1_paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
 		});
 
 		it('element below the viewport does not get waypoint-once class', function () {
-			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
+			expect(document.querySelector('#fixture1_paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
 		});
 
 
@@ -54,12 +48,11 @@ window.addEventListener('DOMContentLoaded', function () {
 
 		beforeEach(function (done) {
 
-			window.location.hash = 'paragraph_1';
 			Arrive.reset();
-			setUpHTMLFixture();
-			Arrive.register_selector('#paragraph_20');
-			Arrive.register_selector('#paragraph_40');
-			window.location.hash = 'paragraph_19';
+			setUpHTMLFixture('fixture2_');
+			Arrive.register_selector('#fixture2_paragraph_20');
+			Arrive.register_selector('#fixture2_paragraph_40');
+			window.location.hash = 'fixture2_paragraph_20';
 
 			setTimeout(function () {
 				done();
@@ -68,11 +61,11 @@ window.addEventListener('DOMContentLoaded', function () {
 		});
 
 		it('added to element when it becomes visible', function () {
-			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-visible');
+			expect(document.querySelector('#fixture2_paragraph_20').className.split(/\s+/)).toContain('waypoint-visible');
 		});
 
 		it('element below the viewport does not get waypoint-visible class', function () {
-			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
+			expect(document.querySelector('#fixture2_paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
 		});
 
 	});
@@ -81,17 +74,16 @@ window.addEventListener('DOMContentLoaded', function () {
 
 		beforeEach(function (done) {
 
-			window.location.hash = 'paragraph_1';
 			Arrive.reset();
-			setUpHTMLFixture();
-			Arrive.register_selector('#paragraph_20');
-			Arrive.register_selector('#paragraph_40');
-			window.location.hash = 'paragraph_19';
+			setUpHTMLFixture('fixture3_');
+			Arrive.register_selector('#fixture3_paragraph_20');
+			Arrive.register_selector('#fixture3_paragraph_40');
+			window.location.hash = 'fixture3_paragraph_20';
 
 			// Give arrive a chance to run
 			setTimeout(function () {
 
-				window.location.hash = 'paragraph_1';
+				window.location.hash = 'fixture3_paragraph_1';
 
 				// Give arrive a chance to run again
 				setTimeout(function () {
@@ -103,15 +95,15 @@ window.addEventListener('DOMContentLoaded', function () {
 		});
 
 		it('removed from element when no longer visible', function () {
-			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).not.toContain('waypoint-visible');
+			expect(document.querySelector('#fixture3_paragraph_20').className.split(/\s+/)).not.toContain('waypoint-visible');
 		});
 
 		it('still has waypoint-once', function () {
-			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
+			expect(document.querySelector('#fixture3_paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
 		});
 
 		it('element below the viewport never had waypoint-visible class', function () {
-			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
+			expect(document.querySelector('#fixture3_paragraph_40').className.split(/\s+/)).not.toContain('waypoint-visible');
 		});
 
 	});
@@ -126,26 +118,25 @@ window.addEventListener('DOMContentLoaded', function () {
 			visible_callback_called = {'20': false, '40': false};
 			no_longer_visible_callback_called = {'20': false, '40': false};
 
-			window.location.hash = 'paragraph_1';
 			Arrive.reset();
-			setUpHTMLFixture();
-			Arrive.register_selector('#paragraph_20', function () {
+			setUpHTMLFixture('fixture4_');
+			Arrive.register_selector('#fixture4_paragraph_20', function () {
 				visible_callback_called['20'] = true;
 			}, function () {
 				no_longer_visible_callback_called['20'] = true;
 			});
-			Arrive.register_selector('#paragraph_40', function () {
+			Arrive.register_selector('#fixture4_paragraph_40', function () {
 				visible_callback_called['40'] = true;
 			}, function () {
 				no_longer_visible_callback_called['40'] = true;
 			});
 
-			window.location.hash = 'paragraph_19';
+			window.location.hash = 'fixture4_paragraph_19';
 
 			// Give arrive a chance to run
 			setTimeout(function () {
 
-				window.location.hash = 'paragraph_1';
+				window.location.hash = 'fixture4_paragraph_1';
 
 				// Give arrive a chance to run again
 				setTimeout(function () {
@@ -184,10 +175,9 @@ window.addEventListener('DOMContentLoaded', function () {
 			visible_callback_called = {'20': false};
 			no_longer_visible_callback_called = {'20': false};
 
-			window.location.hash = 'paragraph_1';
 			Arrive.reset();
-			setUpHTMLFixture();
-			var promise = Arrive.register_selector('#paragraph_20');
+			setUpHTMLFixture('fixture5_');
+			var promise = Arrive.register_selector('#fixture5_paragraph_20');
 
 			promise.visible_callback = function () {
 				visible_callback_called['20'] = true;
@@ -197,12 +187,12 @@ window.addEventListener('DOMContentLoaded', function () {
 				no_longer_visible_callback_called['20'] = true;
 			};
 
-			window.location.hash = 'paragraph_19';
+			window.location.hash = 'fixture5_paragraph_20';
 
 			// Give arrive a chance to run
 			setTimeout(function () {
 
-				window.location.hash = 'paragraph_1';
+				window.location.hash = 'fixture5_paragraph_1';
 
 				// Give arrive a chance to run again
 				setTimeout(function () {
@@ -227,11 +217,10 @@ window.addEventListener('DOMContentLoaded', function () {
 
 		beforeEach(function (done) {
 
-			window.location.hash = 'paragraph_1';
 			Arrive.reset();
-			setUpHTMLFixture();
-			Arrive.register_element(document.querySelector('#paragraph_20'));
-			window.location.hash = 'paragraph_19';
+			setUpHTMLFixture('fixture6_');
+			Arrive.register_element(document.querySelector('#fixture6_paragraph_20'));
+			window.location.hash = 'fixture6_paragraph_20';
 
 			setTimeout(function () {
 				done();
@@ -240,11 +229,11 @@ window.addEventListener('DOMContentLoaded', function () {
 		});
 
 		it('added to element when it becomes visible', function () {
-			expect(document.querySelector('#paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
+			expect(document.querySelector('#fixture6_paragraph_20').className.split(/\s+/)).toContain('waypoint-once');
 		});
 
 		it('element below the viewport does not get waypoint-once class', function () {
-			expect(document.querySelector('#paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
+			expect(document.querySelector('#fixture6_paragraph_40').className.split(/\s+/)).not.toContain('waypoint-once');
 		});
 
 	});
@@ -253,9 +242,8 @@ window.addEventListener('DOMContentLoaded', function () {
 
 		beforeEach(function (done) {
 
-			window.location.hash = 'paragraph_1';
 			Arrive.reset();
-			setUpHTMLFixture();
+			setUpHTMLFixture('fixture7_');
 			done();
 
 		});
@@ -265,7 +253,7 @@ window.addEventListener('DOMContentLoaded', function () {
 			var exception_received = false;
 
 			try {
-				Arrive.register_selector(document.getElementById('paragraph_30'));
+				Arrive.register_selector(document.getElementById('fixture7_paragraph_30'));
 			}
 			catch (e) {
 				exception_received = true;
